3a971b99bc7253fbd7b639bb32c1ea6e2012d016
Heloísa Saldanha <heloisa.saldanha@gmail.com>
2021-06-08 17:31:11 -0300

    Hello world

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.htm
+
+  root to: "static_pages#index"  # colocar o nome do controller # nome do método
+
 end
