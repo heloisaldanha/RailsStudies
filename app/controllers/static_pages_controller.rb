8900f892213a90e0d10a81f15c114abf8f6858a6
Heloísa Saldanha <heloisa.saldanha@gmail.com>
2021-06-15 17:38:41 -0300

    paginas estaticas

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,4 +3,13 @@ class StaticPagesController < ApplicationController
     def index
         @nome = "Heloísa"
     end
+
+    def sobre 
+    end
+
+    def contato 
+    end
+    
+    def novo_contato
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   root to: "static_pages#index"  # colocar o nome do controller # nome do método
 
+  # para visuzlizar as rotas disponíveis na aplicação : rails routes
+
+  get 'sobre', to: 'static_pages#sobre'
+  get 'contato', to: 'static_pages#contato'  # ele vai pegar o método contato e mandar pro arquivo contato que está dentro da pasta static_pages
+  get 'novo_contato', to: 'static_pages#novo_contato'
+  # para visuzlizar as rotas disponíveis na aplicação : rails routes
 end
