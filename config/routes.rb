1f13e7e0b21541f810eb5d53f43dd80f97b9a69a
Heloísa Saldanha <heloisa.saldanha@gmail.com>
2021-06-17 08:56:55 -0300

    inserindo models contact e user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :contacts  # 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.htm
 
   root to: "static_pages#index"  # colocar o nome do controller # nome do método
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_06_16_205834) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "phone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
