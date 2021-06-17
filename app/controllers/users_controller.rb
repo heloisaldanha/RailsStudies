040d7569b6c63ff5d9f083d4a4e368b75cfaa419
Heloísa Saldanha <heloisa.saldanha@gmail.com>
2021-06-17 15:27:15 -0300

    controllers e validações de user - pt 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,19 @@ class UsersController < ApplicationController
     end
 
     def create # vai receber o post pra fazer a criação dessa rota
+        @user = User.new(user_params)  # 
+        if @user.save
+            flash[:success] = 'Usuário cadastrado com sucesso'
+            redirect_to root_url
+        else
+            render 'new'
+        end           
+    end
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :name, :password, :password_digest)
     end
 end
